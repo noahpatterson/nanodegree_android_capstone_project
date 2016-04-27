5e9684448762e97e8339075d8c73673153e7c80d
Noah Patterson <noah@noahpatterson.me>
2016-04-27 07:44:09 -0400

    WIP player added, but crashing on play

--- DestinyCasts/app/src/main/java/me/noahpatterson/destinycasts/EpisodeActivity.java
+++ DestinyCasts/app/src/main/java/me/noahpatterson/destinycasts/EpisodeActivity.java
@@ -1,30 +1,67 @@
 package me.noahpatterson.destinycasts;
 
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
-import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
+import me.noahpatterson.destinycasts.service.PlayerService;
 import me.noahpatterson.destinycasts.data.PodcastContract;
 import me.noahpatterson.destinycasts.data.PodcastDBHelper;
 
 public class EpisodeActivity extends AppCompatActivity {
     private Context context;
+
+//    private ParcelableTrack parcelableTrack = null;
+    private Boolean playing = false;
+    private String playingURL;
+    private int seek = 0;
+    private View fragmentView;
+//    private ArrayList<ParcelableTrack> mParcelableTrackArrayList;
+    private int mCurrentTrackPosition;
+//    private PlayerViewHolder mPlayerViewHolder;
+    private boolean hasService = false;
+    private static final String LOG = "PlayerFragment";
+
+    // Service broacast constants
+    public static final String ACTION_PLAY_TRACK = "me.noahpatterson.destinycasts.PLAY_TRACK";
+    public static final String ACTION_PAUSE_TRACK = "me.noahpatterson.destinycasts.PAUSE_TRACK";
+    public static final String ACTION_SEEK_TRACK = "me.noahpatterson.destinycasts.SEEK_TRACK";
+    public static final String SEEK_POSITION = "seek_position";
+    public static final String TRACK_PREVIEW_URL = "previewURL";
+
+    // onSaveInstantState constants
+    public static final String PLAYING_URL = "playingURL";
+    public static final String CURR_TRACK = "parcelableTrack";
+    public static final String CURRENT_TRACK_LIST_POSITION = "currentPosition";
+    public static final String IS_PLAYING = "playing";
+
+    //episode url
+    private String episodeUrl;
+    private ImageButton playButton;
+    private SeekBar seekBar;
+    private TextView trackTimeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,9 +134,230 @@ public class EpisodeActivity extends AppCompatActivity {
                 final String description = c.getString(descriptionIndex);
                 TextView episodeDetailDescription = (TextView) findViewById(R.id.episodeDetailDescription);
                 episodeDetailDescription.setText(Html.fromHtml(description).toString());
+
+                //url
+                int episodeUrlIndex = c.getColumnIndex(PodcastContract.EpisodeEntry.COLUMN_URL);
+                episodeUrl = c.getString(episodeUrlIndex);
             }
             c.close();
         }
+
+        //setting up player
+        if (savedInstanceState != null) {
+            playing = savedInstanceState.getBoolean(IS_PLAYING, false);
+            playingURL = savedInstanceState.getString(PLAYING_URL, null);
+//            parcelableTrack = savedInstanceState.getParcelable(CURR_TRACK);
+//            mCurrentTrackPosition = savedInstanceState.getInt(CURRENT_TRACK_LIST_POSITION);
+            seek = savedInstanceState.getInt(SEEK_POSITION);
+        } else {
+//            parcelableTrack = getIntent().getParcelableExtra(TopTracksFragment.CURR_TRACK);
+//            mCurrentTrackPosition = getActivity().getIntent().getIntExtra(TopTracksFragment.CURRENT_TRACK_LIST_POSITION, 0);
+        }
+
+        // here we make sure only to start 1 playerService at a time
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+
+            // if the service exists, don't start another one
+            if (PlayerService.class.getName().equals(service.service.getClassName())) {
+                Log.d(LOG, "service is running");
+                hasService = true;
+            }
+        }
+        // otherwise start a new service
+        if (!hasService) {
+            Intent startPlayerService = new Intent(this, PlayerService.class);
+            startPlayerService.putExtra(TRACK_PREVIEW_URL, episodeUrl);
+            startService(startPlayerService);
+        }
+
+        //assign trackDuration
+        //TODO: this should really be the preview track length, possibly obtained from mediaPlayer
+
+        // make sure the play button is in the right state
+        playButton = (ImageButton) findViewById(R.id.playerPlayButton);
+        if (playing) {
+            playButton.setImageResource(android.R.drawable.ic_media_pause);
+        } else {
+            playButton.setImageResource(android.R.drawable.ic_media_play);
+        }
+
+        // set playbutton click listener
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playTrack();
+            }
+        });
+
+        //set seekBar change listener
+        //assign trackDuration
+        //TODO: this should really be the preview track length, possibly obtained from mediaPlayer
+        seekBar = (SeekBar) findViewById(R.id.playerSeekBar);
+        seekBar.setMax(30 * 1000);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+
+                    String formattedDuration = new SimpleDateFormat("mm:ss").format(new Date(progress));
+                    trackTimeTextView = (TextView) findViewById(R.id.playerCurrentTrackPosition);
+                    trackTimeTextView.setText(formattedDuration);
+
+                    // if the track is playing we need to tell the playerService to scrub the track
+                    if (playing) {
+                        Intent intent = new Intent(ACTION_SEEK_TRACK);
+                        intent.putExtra(SEEK_POSITION, progress);
+                        LocalBroadcastManager.getInstance(getBaseContext()).sendBroadcast(intent);
+                    }
+
+                    // store how far we seek
+                    seek = progress;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
     }
+
+    @Override
+    public void onStart() {
+        Log.d(LOG, "in onStart");
+        super.onStart();
+
+    }
+
+    @Override
+    public void onResume() {
+        Log.d(LOG, "in onResume");
+        super.onResume();
+
+        // start our PlayerService receivers
+        IntentFilter currPositionFilter = new IntentFilter(PlayerService.ACTION_CURR_POSITION);
+        LocalBroadcastManager.getInstance(this).registerReceiver(currPositionReciever, currPositionFilter);
+
+        IntentFilter playerCompleteFilter = new IntentFilter(PlayerService.ACTION_COMPLETE);
+        LocalBroadcastManager.getInstance(this).registerReceiver(playerCompleteReciever, playerCompleteFilter);
+
+        //auto start track
+        if (!playing && !episodeUrl.equals(playingURL)) {
+            playTrack();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        Log.d(LOG, "in onPause");
+        super.onPause();
+
+        // trash the playerService receivers
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(currPositionReciever);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(playerCompleteReciever);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        Log.d(LOG, "in onSaveInstanceState");
+
+        // save whether the track is playing
+        outState.putBoolean(IS_PLAYING, playing);
+
+        //if we have a track, store it's data
+//        if (parcelableTrack != null) {
+//            outState.putString(PLAYING_URL, parcelableTrack.previewURL);
+//            outState.putParcelable(CURR_TRACK, parcelableTrack);
+//            outState.putInt(CURRENT_TRACK_LIST_POSITION, mCurrentTrackPosition);
+//            outState.putInt(SEEK_POSITION, seek);
+//        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStop() {
+        Log.d(LOG, "in onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(LOG, "in onDestroy");
+        super.onDestroy();
+    }
+
+    public void playTrack() {
+        Log.d(LOG, "in playTrack");
+
+        // if playing and the current selected track's URL matches the PlayingURL we pause the track
+        if (playing && episodeUrl.equals(playingURL)) {
+
+            //pause command
+            Intent intent = new Intent(ACTION_PAUSE_TRACK);
+            LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+
+            // we are no longer playing
+            playing = false;
+
+            // swap the pause button to play
+            playButton.setImageResource(android.R.drawable.ic_media_play);
+
+            // store the track's playing position
+//            seek = mPlayerViewHolder.seekBar.getProgress();
+        }
+
+        // otherwise we start the selected track
+        else {
+            Intent intent = new Intent(ACTION_PLAY_TRACK);
+            intent.putExtra(TRACK_PREVIEW_URL, episodeUrl);
+            intent.putExtra(SEEK_POSITION, seek);
+            LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+            playingURL = episodeUrl;
+            playButton.setImageResource(android.R.drawable.ic_media_pause);
+        }
+    }
+
+    // receives the currently playing tracks seek position every second and updateas the UI
+    private BroadcastReceiver currPositionReciever = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // PlayerService knows what it is playing so we update the view
+//            if (playingURL == null) {
+            playingURL = intent.getStringExtra(PlayerService.PLAYING_URL);
+//            }
+
+            // only update the seekbar and trackTime if we're viewing the playing track
+            if (episodeUrl.equals(playingURL)) {
+                int currPosition = intent.getIntExtra(PlayerService.CURR_TRACK_POSITION, 0);
+
+                playButton.setImageResource(android.R.drawable.ic_media_pause);
+                seekBar.setProgress(currPosition);
+
+                String formattedDuration = new SimpleDateFormat("mm:ss").format(new Date(currPosition));
+                trackTimeTextView.setText(formattedDuration);
+                playing = true;
+
+                //update seek position in real time
+                seek = currPosition;
+
+            }
+        }
+    };
+
+    // knows when the track is finsihed playing. Resets the player UI
+    private BroadcastReceiver playerCompleteReciever = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            playButton.setImageResource(android.R.drawable.ic_media_play);
+            seekBar.setProgress(0);
+            trackTimeTextView.setText("00:00");
+            playing = false;
+            seek = 0;
+        }
+    };
 
 }
---
+++ DestinyCasts/app/src/main/java/me/noahpatterson/destinycasts/service/PlayerService.java
@@ -0,0 +1,224 @@
+package me.noahpatterson.destinycasts.service;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.Random;
+
+import me.noahpatterson.destinycasts.EpisodeActivity;
+
+public class PlayerService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener {
+    public static final String ACTION_CURR_POSITION = "me.noahpatterson.destinycasts.CURR_POSITION";
+    public static final String ACTION_COMPLETE = "me.noahpatterson.destinycasts.CURR_COMPLETE";
+    private MediaPlayer mMediaPlayer;
+    private String playingURL = null;
+    private Thread updaterThread;
+    private Handler handler;
+    private int seek = 0;
+    private String LOG = "player service";
+
+
+    // Intent String Constants
+    public static final String CURR_TRACK_POSITION = "current_track_position";
+    public static final String PLAYING_URL = "playingURL";
+
+    // this controls updating the seekBar and time while playing
+    private void startUpdater() {
+        updaterThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Thread myCurrent = Thread.currentThread();
+                while (!myCurrent.isInterrupted() && updaterThread == myCurrent) {
+                    notifyUpdate();
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        Log.e(LOG, "Error pausing update thread");
+                        break;
+                    }
+                }
+            }
+        });
+        updaterThread.start();
+    }
+
+    private void notifyStart() {
+        startUpdater();
+    }
+
+    // sends a broadcast to the fragment to update seek and track time in real time
+    private void notifyUpdate() {
+        Log.d(LOG, "updating seekbar");
+        if (mMediaPlayer != null) {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                   Intent currPositionIntent = new Intent(ACTION_CURR_POSITION);
+                    currPositionIntent.putExtra(CURR_TRACK_POSITION, mMediaPlayer.getCurrentPosition());
+                    currPositionIntent.putExtra(PLAYING_URL, playingURL);
+                    LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(currPositionIntent);
+                }
+            });
+        }
+    }
+
+    // random name for the service for logging
+    private String name = "PlayerService" + new Random().nextInt();
+
+    @Override
+    public void onCreate() {
+        Log.d(LOG, "in onCreate: " + name);
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setOnErrorListener(this);
+        mMediaPlayer.setOnPreparedListener(this);
+        mMediaPlayer.setOnCompletionListener(this);
+        handler = new Handler();
+
+        // register receivers for the pause, play, and seek actions from the fragment
+        IntentFilter playTrackFilter = new IntentFilter(EpisodeActivity.ACTION_PLAY_TRACK);
+        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(playTrackReciever, playTrackFilter);
+
+        IntentFilter pauseTrackFilter = new IntentFilter(EpisodeActivity.ACTION_PAUSE_TRACK);
+        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(pauseTrackReciever, pauseTrackFilter);
+
+        IntentFilter seekTrackFilter = new IntentFilter(EpisodeActivity.ACTION_SEEK_TRACK);
+        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(seekTrackReciever, seekTrackFilter);
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(LOG, "in onStartCommand: " + name);
+        // moved normal implementation to the broadcast receivers because it seems easier to manipulate
+        String previewURL = intent.getStringExtra(EpisodeActivity.TRACK_PREVIEW_URL);
+        try {
+            mMediaPlayer.setDataSource(previewURL);
+        } catch(IllegalArgumentException e) {
+            Log.e("PlayTrackService start", "malformed url");
+        } catch (IOException e) {
+            Log.e("PlayTrackService start", "track may not exist");
+        }
+
+        mMediaPlayer.prepareAsync(); // prepare async to not block main thread
+        playingURL = previewURL;
+        seek = intent.getIntExtra(EpisodeActivity.SEEK_POSITION, 0);
+        return 1;
+    }
+
+    private void playTrack(Intent intent, String previewURL) {
+        // play request to start an existing track
+        if (playingURL != null && playingURL.equals(previewURL)) {
+            mMediaPlayer.seekTo(intent.getIntExtra(EpisodeActivity.SEEK_POSITION, 0));
+            notifyStart();
+            mMediaPlayer.start();
+        }
+        // otherwise play the newly selected track
+        else
+        {
+            mMediaPlayer.reset();
+            try {
+                mMediaPlayer.setDataSource(previewURL);
+            } catch(IllegalArgumentException e) {
+                Log.e("PlayTrackService start", "malformed url");
+            } catch (IOException e) {
+                Log.e("PlayTrackService start", "track may not exist");
+            }
+
+            mMediaPlayer.prepareAsync(); // prepare async to not block main thread
+            playingURL = previewURL;
+            seek = intent.getIntExtra(EpisodeActivity.SEEK_POSITION, 0);
+        }
+    }
+
+    private BroadcastReceiver playTrackReciever = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(LOG, "in playTrackReciever");
+            String previewURL = intent.getStringExtra(EpisodeActivity.TRACK_PREVIEW_URL);
+
+            playTrack(intent, previewURL);
+        }
+    };
+
+    // receives a pause command from the fragment
+    private BroadcastReceiver pauseTrackReciever = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(LOG, "in pauseTrackReciever");
+            mMediaPlayer.pause();
+            updaterThread.interrupt();
+        }
+    };
+
+    // receives a seek command from the fragment
+    private BroadcastReceiver seekTrackReciever = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(LOG, "in seekTrackReciever");
+            int seekPosition = intent.getIntExtra(EpisodeActivity.SEEK_POSITION,0);
+
+            // only seek if there is a player to seek on
+            if (mMediaPlayer != null) {
+                mMediaPlayer.seekTo(seekPosition);
+            }
+        }
+    };
+
+    /** Called when MediaPlayer is ready */
+    @Override
+    public void onPrepared(MediaPlayer player) {
+        Log.d(LOG, "in onPrepared: "+ name);
+        player.seekTo(seek);
+        notifyStart();
+        player.start();
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        Log.e("PlayTrackService start", "Error: " + what + ", " + extra);
+        return true;
+    }
+    @Override
+
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        Log.d(LOG, "in onCompletion: "+ name);
+        updaterThread.interrupt();
+        playingURL = null;
+        mp.reset();
+
+        // send a track completed broadcast to the fragment
+        Intent playerComplete = new Intent(ACTION_COMPLETE);
+        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(playerComplete);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(LOG, "in onDestroy: "+ name);
+        LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(playTrackReciever);
+        LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(pauseTrackReciever);
+        LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(seekTrackReciever);
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+        if (updaterThread != null) {
+            updaterThread.interrupt();
+        }
+    }
+}
